bd91a4ae9d4f548c06837a0740298372096f9ba4
Eric Zhang <ezhang7708@gmail.com>
2025-07-21 23:19:22 -0400

    some hardcode that should be fixed, also need to tune values for line detection, also should verify code works with other lane angle views, check math tomorrow

--- lane_detection.py
+++ lane_detection.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import random
 
+IMAGE_HEIGHT = 2138
+IMAGE_WIDTH = 3824
+
 def detect_lines(img, threshold1=50, threshold2=150, aperture_size=3, minLineLength=100, maxLineGap=10):
     grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(grayscale_img, threshold1, threshold2, apertureSize=aperture_size)
@@ -28,6 +31,15 @@ def draw_lines(img, lines, color=(0, 255, 0)):
     return img
 
 def get_slopes_intercepts(lines):
+    """
+    Returns slopes and horizontal intercepts with the bottom of an image for a set of images
+
+    Params
+    - lines: list of lines
+
+    Ret
+    - (slopes, intercepts): tuple of slopes then intercepts
+    """
     slopes = []
     intercepts = []
 
@@ -36,9 +48,8 @@ def get_slopes_intercepts(lines):
 
         slopes.append((y2 - y1) / (x2 - x1))
 
-        # slope * (x - x1) + y1 = 0
-        # x = -y1 / slope + x1
-        intercepts.append(-y1 / slopes[-1] + x1)
+        intercepts.append((IMAGE_HEIGHT - y1) / slopes[-1] + x1)
+        # print(line)
 
     return (slopes, intercepts)
 
@@ -47,24 +58,49 @@ def detect_lanes(lines):
 
     slopes, intercepts = get_slopes_intercepts(lines)
 
+    # print(slopes)
+    # print(intercepts)
+    # print(angles)
+    
+    filtered_lines = set()
+    seen = [False] * len(lines)
     for i in range(len(lines)):
-        for j in range(i, len(lines)):
-            angle1 = np.arctan(slopes[i])
-            angle2 = np.arctan(slopes[j])
+        if seen[i]:
+            continue
+        
+        for j in range(i+1, len(lines)):
+            if seen[j]:
+                continue
 
-            ang_sim = abs(angle1 - angle2) <= 10
-            int_sim = abs(intercepts[i] - intercepts[j]) <= 1000
+            int_sim = abs(intercepts[i] - intercepts[j]) <= 50
+            ang_sim = abs((np.arctan(slopes[i]) - np.arctan(slopes[j]) + 2 * np.pi) % (2 * np.pi) - 2 * np.pi) <= 0.1
 
-            if ang_sim and int_sim:
-                lanes.append([lines[i], lines[j]])
-                break
+            if int_sim or ang_sim:
+                seen[j] = True
+        
+        filtered_lines.add(i)
+
+    print(filtered_lines)
+
+    filtered_lines = list(filtered_lines)
+
+    for f in filtered_lines:
+        print(slopes[f], intercepts[f], np.arctan(slopes[f]))
+    
+    for i in range(len(filtered_lines)):
+        for j in range(i+1, len(filtered_lines)):
+            angle1 = np.arctan(filtered_lines[i])
+            angle2 = np.arctan(filtered_lines[j])
+
+            ang_sim = abs((angle1 - angle2 + 360) % 360) <= 0.5
+            int_sim = abs(intercepts[filtered_lines[i]] - intercepts[filtered_lines[j]]) <= 500
+
+            if ang_sim or int_sim:
+                lanes.append([lines[filtered_lines[i]], lines[filtered_lines[j]]])
 
     return lanes
 
 def draw_lanes(img, lanes):
-    if lanes is None:
-        return img
-
     for lane in lanes:
         color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
 
---
+++ lane_following.py
@@ -0,0 +1,41 @@
+from lane_detection import detect_lines, draw_lines, get_slopes_intercepts, detect_lanes, draw_lanes
+
+import cv2
+import numpy as np
+
+IMAGE_HEIGHT = 2138
+IMAGE_WIDTH = 3824
+
+def get_lane_center(lanes):
+    center_intercept = 0
+    center_slope = 0
+    min_dist_center = IMAGE_WIDTH
+
+    for lane in lanes:
+        slopes, intercepts = get_slopes_intercepts(lane)
+
+        curr_slope = np.tan((np.arctan(slopes[0]) + np.arctan(slopes[1])) / 2)
+        curr_intercept = (intercepts[0] + intercepts[1]) / 2
+
+        if abs(IMAGE_WIDTH / 2 - curr_intercept) < min_dist_center:
+            center_slope = curr_slope
+            center_intercept = curr_intercept
+            min_dist_center = abs(IMAGE_WIDTH / 2 - curr_intercept)
+    
+    return (center_intercept, center_slope)
+
+def recommend_direction(center, slope):
+    """
+    Params
+    - Center line intercept
+    - Center line slope, currently unused
+
+    Ret
+    - Direction to move to, left, right, forward
+    """
+    if center < IMAGE_WIDTH / 2 - 50:
+        return "left"
+    elif center > IMAGE_WIDTH / 2 + 50:
+        return "right"
+    else:
+        return "forward"
